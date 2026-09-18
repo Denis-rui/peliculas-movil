128c1240b1a8371f7bd95e2eed559c8622c06363
Denis_Ruiz <denisruime.20@gmail.com>
2026-09-18 10:19:03 -0500

    feat: se ha creado el servicio y hemos llamado a uno

---
+++ src/utils/service/TMDBService.js
@@ -0,0 +1,22 @@
+import { axios } from "axios";
+import React from "react";
+import { TMDB_ACCESS_TOKEN, TMDB_BASE_URL } from "@env";
+
+export const getPopularMovies = async () => {
+  try {
+    const response = await axios.get(`${TMDB_BASE_URL}/movie/popular`, {
+      headers: {
+        Authorization: `Bearer ${TMDB_ACCESS_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+      params: {
+        language: "en-US",
+        page: 1,
+      },
+    });
+
+    return response.data.results;
+  } catch (error) {
+    console.error("Error obteniendo peliculas populares:", error);
+  }
+};
